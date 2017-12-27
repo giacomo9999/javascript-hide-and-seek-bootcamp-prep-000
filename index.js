828ee1c61134bddd13f23c4f110c9840533757e2
James Gary <bucket@earthlink.net>
2017-12-27 14:39:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ function increaseRankBy(n)
     listItems[loop].innerHTML = (Number(listItems[loop].innerHTML) + n).toString();
   }
 }
+
+function deepestChild()
+{
+  var tempNode = document.querySelector("#grand-node");
+  var isParent = true;
+  // console.log (tempNode.innerHTML);
+
+  while (isParent)
+  {
+    var innerNode = tempNode.querySelector("* *");
+      if (innerNode.querySelector("* *") !== null)
+        {
+        // console.log("It's a parent");
+        // console.log (innerNode.innerHTML);
+        tempNode = innerNode;
+        }
+      else
+        {
+          return innerNode.innerHTML;
+        }
+  } // close while loop
+}
