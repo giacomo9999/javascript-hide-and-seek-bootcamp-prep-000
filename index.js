cbad08ba4ca46d3f1636807d617a573257117715
James Gary <bucket@earthlink.net>
2017-12-27 14:46:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,42 @@
+function getFirstSelector(selector)
+{
+  var selOut = document.querySelector(selector);
+  return selOut;
+}
+
+function nestedTarget()
+{
+  var targList = document.querySelectorAll(".target");
+  return targList[0];
+}
+
+function increaseRankBy(n)
+{
+  var listItems = document.querySelectorAll("ul.ranked-list li");
+  for (var loop = 0; loop <= listItems.length-1; loop ++)
+  {
+    listItems[loop].innerHTML = (Number(listItems[loop].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild()
+{
+  var tempNode = document.querySelector("#grand-node");
+  // var isParent = true;
+  // console.log (tempNode.innerHTML);
+
+  while (5 === 5)
+  {
+    var innerNode = tempNode.querySelector("* *");
+      if (innerNode.querySelector("* *") !== null)
+        {
+        // console.log("It's a parent");
+        // console.log (innerNode.innerHTML);
+        tempNode = innerNode;
+        }
+      else
+        {
+          return innerNode;
+        }
+  } // close while loop
+}
